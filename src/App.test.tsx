6bca15da7c2af692f338219db7e31b3bb68382ca
Ajay Saha <orbisaideveloper@gmail.com>
2026-07-31 07:07:07 +0530

    test(core): add jest-dom import to fix toBeInTheDocument matcher error

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,6 @@
 import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import React from 'react';
 import App from './App';
 
